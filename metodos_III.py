3e1053e8573ed6a6b2835c84c8da93a938ea854e
GMargo <gmargossian@itba.edu.ar>
2022-01-01 17:40:50 -0300

    add comments

--- metodos_III.py
+++ metodos_III.py
@@ -2,7 +2,7 @@
 
 class Calculadora:
     def __init__(self,n1,n2):
-        self.suma = n1 + n2
+        self.suma = n1 + n2 #asdasdasd
         self.resta = n1 - n2
         self.producto  = n1 * n2
         self.division = n1 / n2
